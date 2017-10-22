e39a04e667785b492756e6aefad3c325bffa50e9
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-10-21 19:33:37 -0700

    match query to relevant technology

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,34 @@
+// sometimes, axios will finish loading the tech list before Vue mounts
+// so we cache that data here, and load it into vue on mount
+let techs = null;
+
 var app = new Vue({
   el: '#app',
   data: {
     query: ''
   },
+  methods: {
+    loadTechs: function () {
+      axios.get('/api/techs')
+        .then(function (response) { techs = response.data; })
+        .catch(function (error) { console.log(error); });
+    }
+  },
   computed: {
     suggestions: function () {
-      return [
-        this.query,
-        "test"
-      ]
+      if (techs === null) {
+        return []; // if techs haven't loaded yet
+      }
+      let query = this.query.toLowerCase();
+      let matches = Object.keys(techs).filter(function (techKey) {
+        if (techs[techKey].pretty_name.toLowerCase().startsWith(query)) return true;
+        for (alias of techs[techKey].aliases) {
+          if (alias.toLowerCase().startsWith(query)) return true;
+        }
+        return false;
+      });
+      console.log(matches);
+      return matches;
     }
   }
 })
